2594cd48a7eb0ca8ccd079f4ca345945942b37f9
ethanrgibson <ethanrgibson33@gmail.com>
2026-05-07 23:55:13 -0600

    Changed port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { appendDealsToFile, getValidBrands, loadWeeklyTracker, saveWeeklyTracker 
 const { getWeekOfMonth } = require('./utils');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
